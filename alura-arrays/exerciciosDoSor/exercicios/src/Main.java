19fbeb06dff17fb356906eb363bf5daa691cc0ac
Kaillanny Fontana dos Santos <163677901+kai-fontana@users.noreply.github.com>
2025-09-10 11:48:27 -0300

    Add files via upload

---
+++ alura-arrays/exerciciosDoSor/exercicios/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Produto> produtos = new ArrayList<>();
+
+        produtos.add(new Produto("Estojo", 19.50));
+    }
+}
---
+++ alura-arrays/exerciciosDoSor/exercicios/src/Produto.java
@@ -0,0 +1,14 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    Produto[] produtos = new Produto[5];
+
+    produtos
+    /*Produto produto01 = new Produto("Estojo", 19.0);
+    Produto produto02 = new Produto("Caderno", 12.99);
+
+    public Produto(String nome, double preco) {
+    }*/
+
+
+}
